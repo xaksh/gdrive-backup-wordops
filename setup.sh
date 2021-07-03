311e969ece9e13e22e1603e291218ebc7ed18920
Deepak Sharma <TechExhibeo@gmail.com>
2021-07-03 12:26:31 +0530

    Add email notifications support for backups

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Set Server Hostname and Host Node in Backup Script passed in the arguments
-sed -i s/^SERVER_HOSTNAME=.*/SERVER_HOSTNAME=$1/ /root/gdrive-backup-wordops/backup.sh
-sed -i s/^NODE=.*/NODE=$2/ /root/gdrive-backup-wordops/backup.sh
+# Set Server Hostname, Host Node and other email notifcation related details in Backup Script passed in the arguments
+sed -i "s/^SERVER_HOSTNAME=.*/SERVER_HOSTNAME='$1'/" /root/gdrive-backup-wordops/backup.sh
+sed -i "s/^NODE=.*/NODE='$2'/" /root/gdrive-backup-wordops/backup.sh
+sed -i "s/^NOTIFY_TO=.*/NOTIFY_TO='$4'/" /root/gdrive-backup-wordops/backup.sh
+sed -i "s/^FROM=.*/FROM='$5'/" /root/gdrive-backup-wordops/backup.sh
+sed -i "s/^TO=.*/TO='$6'/" /root/gdrive-backup-wordops/backup.sh
 
 # Run cron.sh for adding cronjob
 bash /root/gdrive-backup-wordops/cron.sh "$3"
 
 # Install rclone https://rclone.org
-curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh | bash
 wait
 
 # Link your Google Account to rclone by creating New Remote
