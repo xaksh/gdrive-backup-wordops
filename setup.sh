xaksh/gdrive-backup-cpanel

get $PATH env from shell and set in backup script

setup.sh
@@ -6,6 +6,7 @@ sed -i "s/^NODE=.*/NODE='$2'/" /root/gdrive-backup-cpanel/backup.sh
 sed -i "s/^NOTIFY_TO=.*/NOTIFY_TO='$4'/" /root/gdrive-backup-cpanel/backup.sh
 sed -i "s/^FROM=.*/FROM='$5'/" /root/gdrive-backup-cpanel/backup.sh
 sed -i "s/^TO=.*/TO='$6'/" /root/gdrive-backup-cpanel/backup.sh
+sed -i "s?PATH=.*?PATH=$PATH?" /root/gdrive-backup-cpanel/backup.sh
 
 # Run cron.sh for adding cronjob
 bash /root/gdrive-backup-cpanel/cron.sh "$3"

xaksh/gdrive-backup-wordops

setup.sh
@@ -6,6 +6,7 @@ sed -i "s/^NODE=.*/NODE='$2'/" /root/gdrive-backup-wordops/backup.sh
 sed -i "s/^NOTIFY_TO=.*/NOTIFY_TO='$4'/" /root/gdrive-backup-wordops/backup.sh
 sed -i "s/^FROM=.*/FROM='$5'/" /root/gdrive-backup-wordops/backup.sh
 sed -i "s/^TO=.*/TO='$6'/" /root/gdrive-backup-wordops/backup.sh
+sed -i "s?PATH=.*?PATH=$PATH?" /root/gdrive-backup-wordops/backup.sh
 
 # Run cron.sh for adding cronjob
 bash /root/gdrive-backup-wordops/cron.sh "$3"